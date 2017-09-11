0ec7906ae9abf76848ccd959b684d9acaf3bb92b
Levente Bajczi <bajczilevi@gmail.com>
2017-09-11 08:52:27 +0200

    remove i*i, added function

--- lab2/ubung1.c
+++ lab2/ubung1.c
@@ -2,12 +2,18 @@
 
 /* Schreibt die erste N (angegebene Zahl) auf den Bildschirm aus */
 
+int sqrt_i(int n);
+
 int main(int argc, char** argv){
     int n;
     printf("Geben Sie eine ganze Zahl ein!\n");
     scanf("%d", &n);
     for(int i = 1; i <= n; i++){
-        printf("%d\n", i*i);
+        printf("%d\n", sqrt_i(i));
     }
     return 0;
+}
+
+int sqrt_i(int n){
+    return n*n;
 }
